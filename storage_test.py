63705ae459f48a5b4e4e2c01f63ff020e62cba19
Asmaa Mahmoud Fahmydfzdf <52336576+asmaaMahmoudF@users.noreply.github.com>
2024-05-17 03:04:46 +0300

    test_2

---
+++ storage_test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import json 
+
+# with open('db.json', 'r') as f:
+#     for line in f:
+#          print(line)
+#          print('*' * 20)
+#          print('')
+#          json_obj = json.loads(line)
+#          print(json_obj)
+
+with open('db.json', 'r', encoding = 'utf8') as f:
+    al = json.load(f)
+
+print(al)
+print(type(al))
+print(al.keys())
+for k in al.keys():
+    print(type(k))
+    print(k)
